4cb2a453534fa88467ddfbb2ca650297818aa9af
Yuta Sakai <yuta.cielo@gmail.com>
2025-02-13 00:08:12 +0900

    add a function that upload iamge to google drive

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import io
 import cv2
 from PIL import Image
 import numpy as np
-#import pickle
+from io import BytesIO
 
 # env, flask
 import os
+import base64
 from dotenv import load_dotenv
 from flask import Flask, request, abort
 
@@ -21,19 +22,52 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage,
 )
 
-from io import BytesIO
+# 画像ファイル名出力用
+import datetime
 import random
+import string
 
+# Google Drive関連
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.oauth2 import service_account
+
+# 環境変数の読み込み
 load_dotenv()
-
 CHANNEL_ACCESS_TOKEN=os.environ.get("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET=os.environ.get("CHANNEL_SECRET")
+FOLDER_ID=os.environ.get("FOLDER_ID")
+GOOGLE_CREDENTIALS=os.environ.get("GOOGLE_CREDENTIALS")
 
+# GOogle credentials.jsonを作成
+if GOOGLE_CREDENTIALS:
+    with open("credentials.json", "wb") as f:
+        f.write(base64.b64decode(GOOGLE_CREDENTIALS))
+
+# Flask appの初期設定
 app = Flask(__name__)
 
+# Line BotのACCESS TOKENおよびSECRETのセット
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+
+# サービスアカウントキーのJSONファイル
+SERVICE_ACCOUNT_FILE = "credentials.json"
+
+# Google Drive APIのスコープ
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+
+# GoogleDrive認証
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+)
+
+# GoogleDriveAPIクライアントの作成
+service = build("drive", "v3", credentials=creds)
+
+
+# Flaskの動作確認用
 @app.route("/")
 def hello_world():
     return "hello world!"
@@ -56,6 +90,7 @@ def callback():
     return 'OK'
 
 
+# テキストメッセージが来た時の対応
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
@@ -63,6 +98,7 @@ def handle_message(event):
         TextSendMessage(text='シールの写真をおくってね♪'))
         #TextSendMessage(text=event.message.text+'ってなに？'))
 
+# 画像が送られてきた時の対応
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
     message_id = event.message.id
@@ -75,11 +111,21 @@ def handle_image(event):
         TextSendMessage(text='今日もお疲れ様でした♪\n集計結果はこちら！\n'+result)
         )
 
+# 画像からシールの数をカウントする関数
 def count_stickers(image):
     img_bn = io.BytesIO(image)
     img_pil = Image.open(img_bn)
     image = np.asarray(img_pil)
     image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)
+
+    # GoogleDrive保存用のファイルを一時的に保存 
+    now = datetime.datetime.now()
+    time_str = now.strftime("%Y%m%d_%H%M%S")
+    random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    tmp_img_filename = './' + time_str + '_' + random_str + '.jpg'
+    cv2.imwrite(tmp_img_filename, image)
+    upload_file(tmp_img_filename, FOLDER_ID)
+    os.remove(tmp_img_filename)
     
     # 画像サイズを取得
     height, width, _ = image.shape
@@ -147,6 +193,20 @@ def count_stickers(image):
 
     return result_str
 
+def upload_file(file_path, folder_id=None):
+    """Google Drive にファイルをアップロード"""
+    file_metadata = {"name": file_path.split("/")[-1]}
+    
+    if folder_id:
+        file_metadata["parents"] = [folder_id]
+    
+    media = MediaFileUpload(file_path, mimetype="image/jpeg")  # 画像のMIMEタイプ変更可
+    file = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+    
+    return file.get("id")
+
+
+# Flask app の起動
 if __name__ == "__main__":
     app.run()
 
